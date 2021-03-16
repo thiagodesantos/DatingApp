59c4894f9fafa04b1fe3fe8b8f90b2c44f28be39
thiagodesantos <thiagodesantos23@gmail.com>
2021-03-16 09:10:07 -0300

    Implementação do curtir

---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class UserLike
+    {
+        public AppUser SourceUser { get; set; }
+        public int SourceUserId { get; set; }
+        public AppUser LikeUser { get; set; }
+        public int LikedUserId { get; set; }
+    }
+}
