29caf11c76c5e64ebbc0b1e002cae4af49713a24
thiagodesantos <thiagodesantos23@gmail.com>
2021-07-23 17:03:28 -0300

    Ajuste mafilesize teste

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -58,7 +58,7 @@ export class PhotoEditorComponent implements OnInit {
       allowedFileType: ['image'],
       removeAfterUpload: true,
       autoUpload: false,
-      maxFileSize: 10 * 1024 * 1024
+      maxFileSize: 35 * 1024 * 1024
     });
 
     this.uploader.onAfterAddingFile = (file) => {
